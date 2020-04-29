44738d47ac716e4ef7b67b473dbafadd6f5522d4
dev1lfromhell <emercomsvz@gmail.com>
2020-04-29 18:24:09 +0300

    Первое Задание

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,23 @@
-console.log("arr" + " - object");
-console.log(4567 + " - object");
+var money = prompt("Ваш бюджет на месяц", "");
+var time = prompt("Введите дату в формате YYYY-MM-DD", "");
+var budget = "бюджет";
+
+var answer1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+var answer2 = prompt("Во сколько обойдется?", ""); 
+
+var appData = {
+    budget: money,
+    timedata: time,
+    expenses: {answer1: answer2},
+    optionalExpenses: {},
+    income: [],
+    savings: false,
+};
+
+alert("Бюджет на 1 день = " + (money - answer2) / 30 + " " + "рублей");
+console.log (appData.budget);
+console.log (appData.timedata);
+console.log (appData.expenses);
+console.log (appData.optionalExpenses);
+console.log (appData.income);
+console.log (appData.savings);
